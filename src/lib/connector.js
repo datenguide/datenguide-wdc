11414e128e435715872bc1584670940dca6176d0
Christian Rijke <hello@christianrijke.de>
2019-03-26 13:05:31 +0100

    Query with apollo client, get rid of jQuery

--- src/lib/connector.js
+++ src/lib/connector.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef */
-// Create the connector object
+import ApolloClient from 'apollo-client'
+import gql from 'graphql-tag'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import fetch from 'unfetch'
+
 const myConnector = tableau.makeConnector()
 
-// Define the schema
 myConnector.getSchema = schemaCallback => {
   const cols = [
     {
@@ -35,33 +39,32 @@ myConnector.getSchema = schemaCallback => {
   schemaCallback([tableSchema])
 }
 
-// Download the data
-myConnector.getData = (table, doneCallback) => {
-  $.ajax({
-    url: 'http://localhost:3030/graphql',
-    contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({
-      query: `
-     {
-  region(id: "11") {
-    id
-    name
-    WAHL09(filter: { PART04: { nin: [] } }) {
-      value
-      year
-      PART04
-    }
-  }
-}
-`
+myConnector.getData = async (table, doneCallback) => {
+  // custom initialization to make Tableau happy
+  const client = new ApolloClient({
+    link: createHttpLink({
+      uri: 'http://localhost:3030/graphql',
+      fetch
     }),
-    success: result => {
-      console.log('result.data.region.WAHL09', result.data.region.WAHL09)
-      table.appendRows(result.data.region.WAHL09)
-      doneCallback()
-    }
+    cache: new InMemoryCache()
   })
+  const result = await client.query({
+    query: gql`
+      {
+        region(id: "11") {
+          id
+          name
+          WAHL09(filter: { PART04: { nin: [] } }) {
+            value
+            year
+            PART04
+          }
+        }
+      }
+    `
+  })
+  table.appendRows(result.data.region.WAHL09)
+  doneCallback()
 }
 
 tableau.registerConnector(myConnector)
